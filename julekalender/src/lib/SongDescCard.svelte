37fd6ba7837faedff4cb4f588a76f4f60c5bddf1
ambosch <anne.bosch@soprasteria.com>
2023-12-04 11:03:08 +0100

    Update responsiveness and song descriptions

--- julekalender/src/lib/SongDescCard.svelte
+++ julekalender/src/lib/SongDescCard.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <div
-  class="rounded-xl bg-rose-100 mx-4 max-h-80 overflow-y-hidden hover:overflow-y-auto"
+  class="rounded-xl bg-rose-100 mx-4 max-h-60 lg:max-h-80 overflow-y-auto lg:overflow-y-hidden hover:overflow-y-auto"
 >
   <StyledP className="font-bold text-left pl-5">{day_nr}. desember</StyledP>
   <!-- <StyledP className="text-left pl-5">Sangtittel - Artist</StyledP> -->
-  <StyledP className="text-left pl-5 pb-5 pr-5"><slot /></StyledP>
+  <StyledP className="text-left pl-5 pb-5 pr-5 "><slot /></StyledP>
 </div>
