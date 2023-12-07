71160ad62be4162fbe3baf9743ddaba6015ad011
ambosch <anne.bosch@soprasteria.com>
2023-12-07 10:34:58 +0100

    Update desktop layout to grid

--- julekalender/src/App.svelte
+++ julekalender/src/App.svelte
@@ -6,10 +6,10 @@
 <main class="container flex flex-col gap-y-8">
   <TopDescription />
   <div
-    class="flex gap-y-10 lg:gap-y-0 lg:gap-x-10 flex-col lg:flex-row lg:h-screen"
+    class="flex gap-y-10 lg:gap-y-0 lg:gap-x-10 flex-col lg:grid lg:grid-cols-5 lg:h-screen"
   >
     <iframe
-      class="min-h-[384px] lg:flex-1"
+      class="min-h-[384px] lg:col-span-2"
       width="100%"
       height="100%"
       title="spotify"
@@ -19,6 +19,6 @@
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       loading="lazy"
     ></iframe>
-    <AboutTheSongs className="min-h-[384px] lg:flex-1" />
+    <AboutTheSongs className="min-h-[384px] lg:col-span-3" />
   </div>
 </main>
